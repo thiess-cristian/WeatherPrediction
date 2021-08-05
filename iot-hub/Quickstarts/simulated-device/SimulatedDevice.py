4f1b816d95fce8b20fd4108317a6ce3b4193e0f4
cristi <thiess.cristi@gmail.com>
2021-08-05 18:11:07 +0300

    idk

--- iot-hub/Quickstarts/simulated-device/SimulatedDevice.py
+++ iot-hub/Quickstarts/simulated-device/SimulatedDevice.py
@@ -9,15 +9,23 @@ import time
 # The sample connects to a device-specific MQTT endpoint on your IoT Hub.
 from azure.iot.device import IoTHubDeviceClient, Message
 
-# The device connection string to authenticate the device with your IoT hub.
-# Using the Azure CLI:
-# az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
-CONNECTION_STRING = "{Your IoT hub device connection string}"
+import requests
+import os
+from datetime import datetime
 
-# Define the JSON message to send to IoT Hub.
-TEMPERATURE = 20.0
-HUMIDITY = 60
-MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity}}}'
+lat="45.657974"
+lon="25.601198"
+key="3c8ed3ac65cb597f0d30d0c07259b6ff"
+CONNECTION_STRING = "HostName=WeatherData.azure-devices.net;DeviceId=DataSender;SharedAccessKey=FChK/5Ls4g4a37TaMp8jhQdsXjyQwILDFHAhieM+Ymw="
+
+def call_api():
+    #complete_api_link = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid="+key
+    complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q=brasov&appid="+key
+
+    api_link = requests.get(complete_api_link)
+    api_data = api_link.json()
+
+    return api_data
 
 def iothub_client_init():
     # Create an IoT Hub client
@@ -32,23 +40,16 @@ def iothub_client_telemetry_sample_run():
 
         while True:
             # Build the message with simulated telemetry values.
-            temperature = TEMPERATURE + (random.random() * 15)
-            humidity = HUMIDITY + (random.random() * 20)
-            msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity)
-            message = Message(msg_txt_formatted)
-
-            # Add a custom application property to the message.
-            # An IoT hub can filter on these properties without access to the message body.
-            if temperature > 30:
-              message.custom_properties["temperatureAlert"] = "true"
-            else:
-              message.custom_properties["temperatureAlert"] = "false"
+            
+            api_data=call_api()
+            
+            message = Message(str(api_data))
 
             # Send the message.
             print( "Sending message: {}".format(message) )
             client.send_message(message)
             print ( "Message successfully sent" )
-            time.sleep(1)
+            time.sleep(20)
 
     except KeyboardInterrupt:
         print ( "IoTHubClient sample stopped" )
